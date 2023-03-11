3a409fb4ff2f4a20fd27776fa1a53b01fd260b87
YooSangbum <ysbjjyes@gmail.com>
2023-03-11 21:14:33 +0900

    2023.03.11

--- js/common.js
+++ js/common.js
@@ -1,7 +1,31 @@
-const $gnb = document.querySelector('.gnb');
-const $ham = document.querySelector('.ham');
+/* 반응형 ham버튼 */
+const $gnb = document.querySelector(".gnb");
+const $ham = document.querySelector(".ham");
 
-$ham.addEventListener('click', () => {
-  $gnb.classList.toggle('active');
-  $ham.classList.toggle('on');
+$ham.addEventListener("click", () => {
+  $gnb.classList.toggle("active");
+  $ham.classList.toggle("on");
+});
+
+/* 반응형 submenu 나오기 */
+const menuList = document.querySelectorAll(".gnb.active > li > a");
+console.log(menuList);
+
+menuList.forEach((mainmenu) => {
+  mainmenu.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    console.log(e.target);
+
+    let eTarget = e.target;
+
+    if (eTarget.classList.contains("on") == false) {
+      menuList.forEach((item) => {
+        item.classList.remove("on");
+      });
+      eTarget.classList.add("on");
+    } else {
+      eTarget.classList.remove("on");
+    }
+  });
 });
